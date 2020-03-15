8c59b9a3067ff6668e5ff7cb188df89a08f59a9b
Mohibb Malik <mohibb91@gmail.com>
2020-03-15 17:24:11 +0100

    Added connection test

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,4 +1,6 @@
 import serial.tools.list_ports
+import random
+import string
 
 
 def get_port(text, port=None):
@@ -16,5 +18,32 @@ def get_port(text, port=None):
     return ports[port].device
 
 
+def test_connection():
+    baudrate = 57600
+
+    ser_send = get_port("Sending")
+    ser_rec = get_port("Receiving")
+
+    with serial.Serial(ser_send, baudrate, timeout=1) as send:
+        with serial.Serial(ser_rec, baudrate, timeout=1) as receive:
+            send.flushOutput()
+            receive.flushInput()
+
+            N = random.randint(50, 100)
+            send_message = ''.join(random.choice(string.printable) for _ in range(N))
+
+            send.write(send_message.encode())
+            rec_message = receive.read(len(send_message)).decode()
+
+    print("Sent Message:   \t", send_message)
+    print("Receive Message:\t", rec_message)
+    print()
+
+    if send_message == rec_message:
+        print("Connection operational")
+    else:
+        print("Connection failed")
+
+
 if __name__ == '__main__':
-    print(get_port("Testing:"))
+    test_connection()
